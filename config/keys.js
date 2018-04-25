0e5bf4e3cd0ce3aa65ef3047cfb9e86244565f3a
Jordany <jordanyryan@gmail.com>
2018-04-25 19:22:00 -0400

    Connected mongoose to mLab

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb://jordany:social-dev-jd@ds259119.mlab.com:59119/social-dev-jd'
+};
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// DB config
+
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+	.connect(db)
+	.then(() => console.log('MongoDB connected'))
+	.catch(err => console.log(err));
+
+app.get('/', (req,res) => res.send('hello world'));
 
 const port = process.env.port || 5000;
 
