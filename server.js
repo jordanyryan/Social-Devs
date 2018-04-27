1d9f1e13842b513105143f76c2b98613c950cc1c
Jordany <jordanyryan@gmail.com>
2018-04-27 14:09:23 -0400

    Added routers for posts profile and users

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 // DB config
@@ -15,6 +19,12 @@ mongoose
 
 app.get('/', (req,res) => res.send('hello world'));
 
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = process.env.port || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
