a2ed317356691aaf113f9cc7bfccad187e72987d
Jordany <jordanyryan@gmail.com>
2018-04-27 14:55:31 -0400

    Added signed jwt on login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://jordany:social-dev-jd@ds259119.mlab.com:59119/social-dev-jd'
+	mongoURI: 'mongodb://jordany:social-dev-jd@ds259119.mlab.com:59119/social-dev-jd',
+	secretOrKey: 'secret stuff'
 };
