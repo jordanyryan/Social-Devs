0bcc149fae25c424f0b07c5650a0605398650d8a
Jordany <jordanyryan@gmail.com>
2018-05-05 16:25:16 -0400

    Added route to like and unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -60,21 +60,60 @@ router.get('/:post_id', (req, res) => {
 
 router.delete('/:post_id', passport.authenticate('jwt', {session: false}), (req, res) => {
 
-	Profile.findOne({user: req.user.id})
-		.then(profile => {
-			Post.findById(req.params.post_id)
-				.then(post => {
-					// Check for post owner
-					if(post.user.toString() !== req.user.id) {
-						return res.status(401).json({notauthorized: 'User not authorized'});
-					}
+	Post.findById(req.params.post_id)
+		.then(post => {
+			// Check for post owner
+			if(post.user.toString() !== req.user.id) {
+				return res.status(401).json({notauthorized: 'User not authorized'});
+			}
 
-					post.remove()
-						.then(() => res.json({success: true}))
-						.catch(err => res.status(404).json({postnotfound: 'No post found'}));
-				})
+			post.remove()
+				.then(() => res.json({success: true}))
+				.catch(err => res.status(404).json({postnotfound: 'No post found'}));
 		})
+		.catch(err => res.status(404).json({postnotfound: 'No post found'}));
 })
+
+// @route POST api/posts/like/:post_id
+// @desc Like/Unlike a Post
+// @access Private
+
+router.post('/:post_id/like', passport.authenticate('jwt', {session: false}), (req, res) => {
+	Post.findById(req.params.post_id)
+		.then(post => {
+			const indexOfUser = post.likes.findIndex(like => like.user.toString() === req.user.id);
+			indexOfUser === -1 ? post.likes.unshift({user: req.user.id}) : post.likes.splice(indexOfUser, 1);
+			post.save()
+				.then(post => res.json(post));
+		})
+		.catch(err => res.status(404).json({postnotfound: 'No post found'}));
+})
+
+router.post('/:post_id/comment', passport.authenticate('jwt', {session: false}), (req, res) => {
+	const {errors, isValid} = validatePostInput(req.body);
+	// Check valid
+
+	if(!isValid) {
+		return res.status(400).json(errors);
+	}
+
+	Post.findById(req.params.post_id)
+		.then(post => {
+			const {text, name, avatar} = req.body;
+			const user = req.user.id;
+			post.comments.unshift({text,name,avatar,user});
+			post.save()
+				.then(post => res.json(post));
+		})
+		.catch(err => res.status(404).json({postnotfound: 'No post found'}));
+})
+
+
+
+// @route POST api/posts/unlike/:post_id
+// @desc Unlike a Post
+// @access Private
+
 
 
 module.exports = router;
