a094576b0c580d2dfc724bc0a61454b744b052f2
Jordany <jordanyryan@gmail.com>
2018-05-11 18:51:51 -0400

    Added redux to client and added redux chrome extension

--- validation/register.js
+++ validation/register.js
@@ -3,16 +3,18 @@ const {isEmpty, checkMultipleEmpty} = require('./helpers');
 
 module.exports = function validateRegisterInput(data) {
 	let errors = {};
-	checkMultipleEmpty(['name', 'email', 'password', 'password2'], data, errors);
+	if (!Validator.isEmail(data.email)) {
+	    errors.email = 'Email is invalid';
+	  }
+	checkMultipleEmpty(['name', 'email', 'password'], data, errors);
 
 	if(!Validator.isLength(data.name, {min: 2, max: 30})) {
-		errors.name = 'Name must be between 2 and 30 characters';
+		if (!errors.name) errors.name = 'Name must be between 2 and 30 characters';
 	}
 
 	if(!Validator.isLength(data.password, {min: 6, max: 30})) {
-		errors.password = 'Password must be at least 6 characters';	
+		if (!errors.password) errors.password = 'Password must be at least 6 characters';	
 	}
-
 
 
 	if(Validator.isEmpty(data.password2)) {
@@ -20,7 +22,7 @@ module.exports = function validateRegisterInput(data) {
 	}
 
 	if(!Validator.equals(data.password, data.password2)) {
-		errors.password2 = 'Passwords must match';	
+		if (!errors.password2) errors.password2 = 'Passwords must match';	
 	}
 
 	return {
