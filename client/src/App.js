8c5127d43ed3b7ea9caffa280bb35a92e50035a1
Jordany <jordanyryan@gmail.com>
2018-05-19 16:09:23 -0400

    Created a profile component to showcase a user's whole profile by handle

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 // Auth components
 import Register from './components/auth/Register';
@@ -63,6 +64,7 @@ class App extends Component {
 					<Route exact path="/register" component={Register} />
 					<Route exact path="/login" component={Login} />
 					<Route exact path="/profiles" component={Profiles} />
+					<Route exact path="/profile/:handle" component={Profile} />
 					<Switch>
 						<PrivateRoute exact path="/dashboard" component={Dashboard} />
 					</Switch>
---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import ProfileHeader from './ProfileHeader';
+import ProfileAbout from './ProfileAbout';
+import ProfileCreds from './ProfileCreds';
+import ProfileGithub from './ProfileGithub';
+import Spinner from '../common/spinner';
+import { getProfileByHandle } from '../../actions/profileActions';
+
+class Profile extends Component {
+	componentDidMount() {
+		if(this.props.match.params.handle) {
+			this.props.getProfileByHandle(this.props.match.params.handle);
+		}
+	}
+
+	render() {
+		const {profile, loading} = this.props.profile;
+		return (
+			<div>
+				<ProfileHeader />
+				<ProfileAbout />
+				<ProfileCreds />
+				<ProfileGithub />
+			</div>
+		)
+	}
+}
+
+Profile.propTypes = {
+	getProfileByHandle: PropTypes.func.isRequired,
+	profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = ({profile}) => ({
+	profile
+})
+
+export default connect(mapStateToProps, {getProfileByHandle})(Profile);
