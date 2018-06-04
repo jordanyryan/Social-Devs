09fa08abe2998b9997d4e234d958d50505833373
Jordany <jordanyryan@gmail.com>
2018-06-04 12:48:46 -0400

    Added types to action, and created a postReducer

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,14 @@
+import {GET_ERRORS} from '../actions/types';
+
+const initialState = {
+	posts: [],
+	post: {},
+	loading: false
+};
+
+export default function(state = initialState, action) {
+	switch(action.type) {
+		default:
+			return state;
+	}
+}
