4ed000d214abeb2dab31df95990d6c282d262c79
Jordany <jordanyryan@gmail.com>
2018-06-25 15:38:22 -0400

    Added a clear errors action

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-import {ADD_POST, GET_POSTS, POST_LOADING, DELETE_POST, GET_ERRORS, GET_POST} from './types';
+import {ADD_POST, GET_POSTS, POST_LOADING, DELETE_POST, GET_ERRORS, GET_POST, CLEAR_ERRORS} from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
+	dispatch(clearErrors());
 	axios.post('/api/posts', postData)
 		.then(res => 
 			dispatch({
@@ -88,6 +89,7 @@ export const likePost = postId => dispatch => {
 
 // Add Comment
 export const addComment = (postId, commentData) => dispatch => {
+	dispatch(clearErrors());
 	axios.post(`/api/posts/${postId}/comment`, commentData)
 		.then(res => 
 			dispatch({
@@ -127,6 +129,14 @@ export const deleteComment = (postId, commentId) => dispatch => {
 export const setPostLoading = () => {
 	return {
 		type: POST_LOADING
+	}
+}
+
+// Clear Errors
+
+export const clearErrors = () => {
+	return {
+		type: CLEAR_ERRORS
 	}
 }
 
