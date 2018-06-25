33b23af927999666a0aa40cd86b4d29e191dbdf0
Jordany <jordanyryan@gmail.com>
2018-06-25 15:19:06 -0400

    Added a comment form component that allows a user to submit a comment to a post

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -86,6 +86,23 @@ export const likePost = postId => dispatch => {
 		);
 }
 
+// Add Comment
+export const addComment = (postId, commentData) => dispatch => {
+	axios.post(`/api/posts/${postId}/comment`, commentData)
+		.then(res => 
+			dispatch({
+				type: GET_POST,
+				payload: res.data
+			})
+		)
+		.catch(err => 
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})	
+		);
+}
+
 // Set Loading
 
 export const setPostLoading = () => {
